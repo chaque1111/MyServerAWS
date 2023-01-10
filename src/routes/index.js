ecae7b066b33f099017104ac2d1f6d8bcbc3ef88
chaque1111 <99604927+chaque1111@users.noreply.github.com>
2023-01-10 19:44:27 -0300

    agregando ruta /

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,9 @@ const Router = require("express");
 const clientes = require("./clientes");
 const router = Router();
 
+router.get("/", (req, res) => {
+  res.send("hola bienvenido a aws");
+});
 router.use("/clientes", clientes);
 
 module.exports = router;
